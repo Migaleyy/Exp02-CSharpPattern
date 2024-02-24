50d673ee5116d5eea04f53498aa28aeec07a3ad7
Migaleyy <migal.arunadann4124@gmail.com>
2024-02-24 10:57:54 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,29 @@ public class Pattern
 {
     public static void Main()
     {
-        // type the code here
+        int n;
+        n=Console.ToInt32(Console.ReadLine("Enter the number of rows:""));
+        for (int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i;j++)
+            {
+                Console.Write(" ");
+                for(int k=0;k<=i;k++)
+                {
+                    if(i==0 || k==0)
+                    {
+                        c=1;
+                    }
+                    else
+                    {
+                        char=char*(i-k+1)/j;
+                        Console.WriteLine(char+" ");
+                    }
+                    }
+                }
+            }
+        }
+
+        
     }
 }
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Exp02-CSharpPattern
 
 ## Aim:
-
+To write a c# program and simulate Pascal's Triangle
 ## Algorithm:
 
 ### Step1:
+Get the number of rows from the user
 ### Step2:
+
 ### Step3:
 ### Step4:
 ### Step5:
